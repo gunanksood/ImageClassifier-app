d65af402523ae42f62ccb2597c59974c931392f1
gunanksood <gunanksood@gmail.com>
2020-07-18 14:21:19 +0530

    updated train and predict files

--- predict.py
+++ predict.py
@@ -1,67 +1,29 @@
-import argparse
+import click
 import json
 import PIL
 import torch
 import numpy as np
 
 from math import ceil
-from train import check_gpu
+from train import gpu_availability
 from torchvision import models
 
 
-def arg_parser():
-    parser = argparse.ArgumentParser(description="Neural Network Settings")
-
-    #image for prediction
-    parser.add_argument('--image', 
-                        type=str, 
-                        help='Point to image file for prediction.',
-                        required=True)
-
-    # Loads checkpoint created by train.py
-    parser.add_argument('--checkpoint', 
-                        type=str, 
-                        help='Point to checkpoint file',
-                        required=True)
-    
-    # Specify top-k
-    parser.add_argument('--top_k', 
-                        type=int, 
-                        help='Choose top K matches')
-    
-    # Import category names
-    parser.add_argument('--category_names', 
-                        type=str, 
-                        help='Mapping from categories to real names.')
-
-    parser.add_argument('--gpu', 
-                        action="store_true", 
-                        help='Use GPU')
-
-    args = parser.parse_args()
-    
-    return args
-
-
-def load_checkpoint(checkpoint_path):
+def checkpoint_loader(checkpoint_path):
     # Load the saved file
     checkpoint = torch.load("my_checkpoint.pth")
     
-    # Load Defaults if none specified
-    if checkpoint['architecture'] == 'vgg16':
-        model = models.vgg16(pretrained=True)
-        model.name = "vgg16"
-    else: 
-        exec("model = models.{}(pretrained=True)".checkpoint['architecture'])
-        model.name = checkpoint['architecture']
+    model = eval("models.{}(pretrained=True)".format(checkpoint['architecture']))
+    model.name = checkpoint['architecture']
     
-    for param in model.parameters(): param.requires_grad = False
+    for param in model.parameters(): 
+        param.requires_grad = False
     
     # Load stuff from checkpoint
     model.class_to_idx = checkpoint['class_to_idx']
     model.classifier = checkpoint['classifier']
     model.load_state_dict(checkpoint['state_dict'])
-    
+  
     return model
 
 
@@ -71,19 +33,19 @@ def process_image(image_path):
     orig_width, orig_height = test_image.size
 
     # Finding shorter size and create settings to crop shortest side to 256
-    if orig_width < orig_height: resize_size=[256, 256**600]
-    else: resize_size=[256**600, 256]
+    if orig_width < orig_height: 
+        resize_size=[256, 256**600]
+    else: 
+        resize_size=[256**600, 256]
         
     test_image.thumbnail(size=resize_size)
 
-    # Find pixels to crop on to create 224x224 image
     center = orig_width/4, orig_height/4
     left, top, right, bottom = center[0]-(244/2), center[1]-(244/2), center[0]+(244/2), center[1]+(244/2)
     test_image = test_image.crop((left, top, right, bottom))
 
     # Converrt to numpy - 244x244 image w/ 3 channels (RGB)
-    np_image = np.array(test_image)# /255  Divided by 255 because imshow() expects integers (0:1)!!
-    print(np_image)
+    np_image = np.array(test_image)
 
     # Normalize each color channel
     normalise_means = [0.485, 0.456, 0.406]
@@ -92,22 +54,11 @@ def process_image(image_path):
 #     np_image = (np_image - normalise_means)
 #     np_image = np_image / normalise_std
         
-    # Set the color to the first channel
     np_image = np_image.transpose(2, 0, 1)
-    
     return np_image
 
 
-def predict(image_tensor, model, device, cat_to_name, top_k):
-    ''' Predict the class (or classes) of an image using a trained deep learning model.
-    
-    image_path: string. Path to image, directly to image and not to folder.
-    model: pytorch neural network.
-    top_k: integer. The top K classes to be calculated
-    
-    returns top_probabilities(k), top_labels
-    '''
-    
+def predict(image_tensors, model, device, category_to_name, top_k):    
     # check top_k
     if type(top_k) == type(None):
         top_k = 5
@@ -116,7 +67,7 @@ def predict(image_tensor, model, device, cat_to_name, top_k):
     # Set model to evaluate
     model.eval();
 
-    torch_image = torch.from_numpy(np.expand_dims(image_tensor, 
+    torch_image = torch.from_numpy(np.expand_dims(image_tensors, 
                                                   axis=0)).type(torch.FloatTensor)
     model=model.cpu()
     # Find probabilities (results) by passing through the function (note the log softmax means that its on a log scale)
@@ -136,7 +87,7 @@ def predict(image_tensor, model, device, cat_to_name, top_k):
     idx_to_class = {val: key for key, val in    
                                       model.class_to_idx.items()}
     top_labels = [idx_to_class[lab] for lab in top_labels]
-    top_flowers = [cat_to_name[lab] for lab in top_labels]
+    top_flowers = [category_to_name[lab] for lab in top_labels]
     
     return top_probs, top_labels, top_flowers
 
@@ -146,25 +97,29 @@ def print_probability(probs, flowers):
     for i, j in enumerate(zip(flowers, probs)):
         print ("Rank {}:".format(i+1),
                "Flower: {}, liklihood: {}%".format(j[1], ceil(j[0]*100)))
+
     
+@click.command()
+@click.option('--category_names', required=True, type=str, help = 'Mapping from categories to real names.')
+@click.option('--image', type=str, required=True, help='Point to image file for prediction.')
+@click.option('--checkpoint', type=str, required=True, help='Point to checkpoint file')
+@click.option('--top_k', type=int, help= 'Choose top K matches')
+@click.option('--gpu', is_flag=True, type=bool, help='Use GPU')    
+def main(category_names, image, checkpoint, top_k, gpu):
+    with open(category_names, 'r') as f:
+        	category_to_name = json.load(f)
 
-def main():
-    args = arg_parser()
-   
-    with open(args.category_names, 'r') as f:
-        	cat_to_name = json.load(f)
-
-    model = load_checkpoint(args.checkpoint)
+    model = checkpoint_loader(checkpoint)
     
     # image processing
-    image_tensor = process_image(args.image)
+    image_tensors = process_image(image)
     
-    device = check_gpu(gpu_arg=args.gpu);
+    device = gpu_availability(gpu_arg=gpu);
     
     # Using `processed_image` to predict the top K most likely classes
-    top_probs, top_labels, top_flowers = predict(image_tensor, model, 
-                                                 device, cat_to_name,
-                                                 args.top_k)
+    top_probs, top_labels, top_flowers = predict(image_tensors, model, 
+                                                 device, category_to_name,
+                                                 top_k)
     
     print_probability(top_flowers, top_probs)
 
